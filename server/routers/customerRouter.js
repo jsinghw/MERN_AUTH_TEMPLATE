1753a46ff27465be590dd60462ff4710ae18f05d
Jaspal Singh <jsinghw94@gmail.com>
2021-06-17 22:35:27 -0400

    upload

---
+++ server/routers/customerRouter.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Customer = require("../models/customerModel");
+const auth = require("../middleware/auth");
+
+// new customer
+
+router.post("/", auth, async (req, res) =>{
+    try {
+        const {name} = req.body;
+
+        const newCustomer = new Customer({
+            name
+        })
+
+        const savedCustomer = await newCustomer.save();
+
+        res.json(savedCustomer);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+})
+
+// all customers
+
+router.get("/", auth, async (req, res) => {
+    try {
+        const customers = await Customer.find();
+        res.json(customers)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send()
+    }
+})
+
+module.exports = router;
